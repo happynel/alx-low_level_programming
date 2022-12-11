ea177ad2d836447c09b8a93c68795d1ef314815e
happynel <frankyhappy5050@gmail.com>
2022-12-11 14:26:11 -0800

    Task 2 correct

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 /**
  * main - main  returns alphabets
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch = 'a';
-
+	/* new code */
 	while (ch <= 'z')
+	{
 		putchar(ch);
 		ch++;
+	}
 	putchar('\n');
 	return (0);
 }
