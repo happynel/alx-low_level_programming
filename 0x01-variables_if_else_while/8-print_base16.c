3c5e3fc935844a8758e029532d456d1da5a66f25
happynel <frankyhappy5050@gmail.com>
2022-12-12 11:02:24 -0800

    Modified task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,25 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - returns from 0, followed by new line.
+ * main - print all digit of 
+ * the redix 16 number system
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
+	char letter; 
 
-	char c = 'z';
-
-	while (c >= 'a')
+	int number;
+	
+	for (letter = 'a'; letter <= 'f'; letter++)
 	{
-		putchar(c);
-		c--;
+		while (number <= 9)
+		{
+			putchar(number + '0');
+			number++;
+		}
+		putchar(letter);
 	}
-
 	putchar('\n');
 	return (0);
 }
