b0dd6d385c34b2cf85206c7bb080dac74f250412
happynel <frankyhappy5050@gmail.com>
2022-12-13 07:12:16 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Scuccess)
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+		while (*sh)
+		{
+
+			putchar(*sh);
+			sh++;
+		}
+	putchar('\n');
+	return (0);
+}
