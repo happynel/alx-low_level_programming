6767ba87d448d27b5a9bbeea60e0f738b904e0ee
happynel <frankyhappy5050@gmail.com>
2022-12-13 07:40:10 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= 'z'; n++)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
