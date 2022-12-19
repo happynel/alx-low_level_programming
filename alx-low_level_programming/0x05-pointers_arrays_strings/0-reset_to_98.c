e39436eba8c35b8152e06aba41157f746001398f
happynel <frankyhappy5050@gmail.com>
2022-12-19 13:45:05 -0800

    Update

---
+++ alx-low_level_programming/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 0- a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: logout
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
