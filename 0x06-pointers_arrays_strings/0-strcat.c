c4ffd9bc16742ae6bf068869eb3077163c2e66b8
happynel <frankyhappy5050@gmail.com>
2022-12-21 10:05:01 -0800

    My first task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
  * *_strcat - a function that concatenates two strings.
  * @dest: The destination string
@@ -10,18 +9,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char s1[100] = "programming", s2[] = "is awesome";
-	int length = 0, i, j;
+	int a = -1, i;
 
-	while (s1[length] != '\0')
-		++length;
-	{
-		for (j = 0; s2[j] != '\0'; ++j, ++length)
-			s1[length] = s2[j];
-	}
-	s1[length] = s2[j];
-
-	s1[length] = '\0';
-	puts(s1);
-	return (0);
+	for (i = 0; dest[i] != '\0';)
+	i++;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - Concatenates two strings
+ * @dest: The destination value
+ * @src: the source value
+ * @n: The limit of the concatenates
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int c)
+{
+	int dlen = 0, j = 0;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+	while(j < n && src[j])
+	{		
+		dest[dlen] = src[j];
+	dlen++;
+	j++;
+
+	dest[dlen + n + 1] = '\0';
+	return (dest);
+}
