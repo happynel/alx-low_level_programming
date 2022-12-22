d8b7216a6cce36eaefc64560dcdc48405ac16493
happynel <frankyhappy5050@gmail.com>
2022-12-22 10:32:37 -0800

    Copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && *(src + i) != '\0')
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		*(dest + i) = *(src + 1);
-		i++;
+		*(dest + i) = *(src + i);
 	}
-	while (i != n)
-		dest[i++] = '\0';
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
 	return (dest);
 }
