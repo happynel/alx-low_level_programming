7ceaa08edb514b431b2ff45977084d6d433dabf9
happynel <frankyhappy5050@gmail.com>
2023-04-10 14:08:50 -0700

    6 hash

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,29 +1,53 @@
-#include <stdlib.h>
 #include "hash_tables.h"
-/**
- * hash_table_delete - delete yo hashes
- * @ht: hash table for deletion
- */
-void hash_table_delete(hash_table_t *ht)
-{
-	hash_node_t *holder, *next;
-	unsigned long int index = 0;
 
-	if ((*ht).size == 0 || !(*ht).array || !ht)
-		return;
-	for (; (*ht).size > index; index++)
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if ((*ht).array[index] != NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			for (holder = (*ht).array[index]; holder != NULL; holder = next)
-			{
-				next = (*holder).next;
-				free((*holder).key);
-				free((*holder).value);
-				free(holder);
-			}
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
-	free((*ht).array);
-	free(ht);
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
